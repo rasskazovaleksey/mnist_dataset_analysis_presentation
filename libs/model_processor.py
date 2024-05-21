6d8c4ca2ae34cddd44b7cda7dac656f753398b9d
alex <rasskazov.aleksey@gmail.com>
2024-05-21 11:56:58 +0200

    Initial commit

---
+++ libs/model_processor.py
@@ -0,0 +1,35 @@
+import json
+
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+
+def read_params(path: str = "data/params.json") -> dict:
+    with open(path) as json_file:
+        data = json.load(json_file)
+
+    return data
+
+
+def create_default_model(name: str):
+    if name == "LogisticRegression":
+        return LogisticRegression()
+    elif name == "KNeighborsClassifier":
+        return KNeighborsClassifier()
+    elif name == "GaussianNB":
+        return GaussianNB()
+    elif name == "DecisionTreeClassifier":
+        return DecisionTreeClassifier()
+    elif name == "RandomForestClassifier":
+        return RandomForestClassifier()
+    elif name == "KMeans":
+        return KMeans()
+    elif name == "AgglomerativeClustering":
+        return AgglomerativeClustering()
+    else:
+        raise ValueError(f"'{name}' is not supported")
